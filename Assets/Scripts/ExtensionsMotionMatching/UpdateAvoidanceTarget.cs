fbd08fc70c5e40274a311c15ec6ccaa990c92760
Reiya Itatani <reiya.itatani@gmail.com>
2023-07-17 23:15:56 +0200

    Add social behavoiur

---
+++ Assets/Scripts/ExtensionsMotionMatching/UpdateAvoidanceTarget.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MotionMatching;
+
+public class UpdateAvoidanceTarget : MonoBehaviour
+{
+    private PathController pathCharacterController;
+    private CapsuleCollider colliderMySelf;
+    
+    void Start(){
+        pathCharacterController = this.transform.parent.GetComponent<PathController>();
+        colliderMySelf = pathCharacterController.agentCollider;
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if(pathCharacterController == null) return;
+        if(!other.Equals(colliderMySelf) && other.gameObject.CompareTag("Agent")) 
+        {
+            if (pathCharacterController.CurrentAvoidanceTarget == null || Vector3.Distance(pathCharacterController.GetCurrentPosition(), pathCharacterController.CurrentAvoidanceTarget.transform.position) > Vector3.Distance(pathCharacterController.GetCurrentPosition(), other.transform.position))
+            {
+                pathCharacterController.CurrentAvoidanceTarget = other.gameObject;
+            }
+        }   
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (pathCharacterController.CurrentAvoidanceTarget!=null && pathCharacterController.CurrentAvoidanceTarget.Equals(other.gameObject))
+        {
+            pathCharacterController.CurrentAvoidanceTarget = null;
+        }
+    }
+}
